977a6caa68affd0631be6b51b1994c843afe5f9e
Rameeshmk <rameeshmk@gmail.com>
2024-01-21 22:27:51 +0300

    validation final

--- html/script.js
+++ html/script.js
@@ -3,10 +3,10 @@ function validateName(){
     let name = document.getElementById('fullname');
 
     if(name.value.trim()== ''){
-       nameError.innerHTML ='fullname cannot be blank'
+       nameError.innerHTML ='*fullname cannot be blank'
        return false
     }else if(name.value.length <4){
-        nameError.innerHTML = 'fullname must not be less than 4 characters'
+        nameError.innerHTML = '*fullname must not be less than 4 characters'
         return false
     }
      else{
@@ -39,16 +39,16 @@ function validatePassword(){
     let regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
     if(password.value.trim()==''){
-        passwordError.innerHTML ='password cannot be blank!!'
+        passwordError.innerHTML ='*password is required!!'
         return false
-    }else if( password.value.length<6 || password.value.length>20){
+    //}else if( password.value.length<6 || password.value.length>20){
           
-        passwordError.innerHTML='password must be between 6 to 16 characters'
-        return false
+       // passwordError.innerHTML='password must be between 6 to 16 characters'
+       // return false
 
     }else if(!password.value.match(regularExpression)){
 
-        passwordError.innerHTML='password should contain atleast one number and one special character'
+        passwordError.innerHTML='*password must have minimum 6 to 16 characters & must have atleast one number and one special character'
         return false
     }else{
         passwordError.innerHTML=''
@@ -62,7 +62,7 @@ function validateConfirmpw(){
     let confirmError = document.getElementById('confirm-error')
 
     if(confirm.value != password.value){
-        confirmError.innerHTML='password mismatch'
+        confirmError.innerHTML='*password mismatch'
         return false
     }else{
         confirmError.innerHTML=''
@@ -74,10 +74,10 @@ function validateUser(){
     let userError= document.getElementById('username-error')
 
     if(userName.value.trim()==''){
-        userError.innerHTML='username cannot be blank!!'
+        userError.innerHTML='*username cannot be blank!!'
         return false
     }else if(userName.value.length<4){
-        userError.innerHTML=' username must not be less than 4 characters'
+        userError.innerHTML='* username must not be less than 4 characters'
         return false
     }else{
         userError.innerHTML=""
@@ -90,10 +90,10 @@ function validateNum(){
     let regExp= /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/
 
     if(number.value.trim()==''){
-        numError.innerHTML='phone number cannot be blank!!'
+        numError.innerHTML='*phone number is required!!'
         return false
     }else if(!number.value.match(regExp)){
-        numError.innerHTML= " not a valid phone number"
+        numError.innerHTML= " *not a valid phone number"
         return false
     }else{
         numError.innerHTML=''
@@ -106,7 +106,7 @@ function validateForm(){
     let submitError= document.getElementById('submit-error')
 
     if(!validateName() || !validateEmail() ||!validatePassword() || !validateConfirmpw() || !validateUser() || !validateNum()){
-        submitError.innerHTML='Invalid Data'
+        submitError.innerHTML='*Invalid Data'
         return false
     }else{
         submitError.innerHTML=""
